232d05ca19aa11bc1a6ab5e71a8c1ae8117c7545
cromanalcaide <cromanalcaide@gmail.com>
2023-01-21 11:42:34 +0000

    first commit, back set

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter(User.email == email, User.password == password, User.is_active == True).first
+
+    if user:
+        access_token = create_access_token(identity = email)
+        response_body = {"email": email,
+                        "access_token": access_token}
+        return jsonify(response_body), 200
+    else:
+        response_body = {"message": "Bad username, you idiot, pass or user inactive"}
+        return jsonify(response_body), 401
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -4,38 +4,70 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Demo = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+useEffect(() => {
+	let myHeaders = new Headers();
+  	myHeaders.append(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3NDI5ODMyOCwianRpIjoiNmU5ODkzODctMGQzOC00Y2EwLTk4ZmMtODZlMjBmZmJjNjAxIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3QyQHRlc3QuY29tIiwibmJmIjoxNjc0Mjk4MzI4LCJleHAiOjE2NzQyOTkyMjh9.q0u1kc4PwB_WbI5WvPHo0krza_W3092jorfauTo_evA"
+  );
+
+  let raw = "";
+
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://3001-4geeksacade-reactflaskh-3tv1758ui84.ws-eu83.gitpod.io/api/private",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}, [])
+
+  
+  return (
+    <div className="container">
+      <ul className="list-group">
+        {store.demo.map((item, index) => {
+          return (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-between"
+              style={{ background: item.background }}
+            >
+              <Link to={"/single/" + index}>
+                <span>Link to: {item.title}</span>
+              </Link>
+              {
+                // Conditional render example
+                // Check to see if the background is orange, if so, display the message
+                item.background === "orange" ? (
+                  <p style={{ color: item.initial }}>
+                    Check store/flux.js scroll to the actions to see the code
+                  </p>
+                ) : null
+              }
+              <button
+                className="btn btn-success"
+                onClick={() => actions.changeColor(index, "orange")}
+              >
+                Change Color
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <br />
+      <Link to="/">
+        <button className="btn btn-primary">Back home</button>
+      </Link>
+    </div>
+  );
 };
